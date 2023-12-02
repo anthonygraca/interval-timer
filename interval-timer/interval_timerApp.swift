ec3f97a57c86e79d4109ee87d83a5a7acafd9183
Anthony Graca <anthonygraca@gmail.com>
2023-12-02 14:48:42 -0800

    first commit

---
+++ interval-timer/interval_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  interval_timerApp.swift
+//  interval-timer
+//
+//  Created by Anthony Graca on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct interval_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
